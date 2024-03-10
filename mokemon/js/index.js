e365372711abd9e7ca312536323024b0f9636dd5
JoshuaMartine <popopotro141@gmail.com>
2024-03-10 14:20:44 -0400

    cambios realizados en el js sobre lo que es la seleccion de mascota y en el html se agrego una nueva id llamada vidas y vidas-enemigas

--- mokemon/js/index.js
+++ mokemon/js/index.js
@@ -1,13 +1,34 @@
-
-
-function selecionarMascotaJugador (){
-    alert("selecionas tu mascota")
+function iniciarJuego() {
+  let botonMascota = document.getElementById("boton-mascota");
+  botonMascota.addEventListener("click", selecionarMascotaJugador);
 }
 
-let botonMascota = document.getElementById('boton-mascota')
-botonMascota.addEventListener('click', selecionarMascotaJugador)
+function selecionarMascotaJugador() {
+  let charmander = document.getElementById("charmander").checked;
+  let squirtle = document.getElementById("squirtle").checked;
+  let venasaur = document.getElementById("venasaur").checked;
+  let wakala = document.getElementById("wakala").checked;
+  let kaka = document.getElementById("kaka").checked;
+  let chipa = document.getElementById("chipa").checked;
 
+  if (charmander == true) {
+    alert("selecionaste a charmander");
+  } else if (squirtle == true) {
+    alert("selecionast a squirtle");
+  } else if (venasaur == true) {
+    alert("selecionaste a venasuar");
+  } else if (wakala == true) {
+    alert("selecionaste a wakala");
+  } else if (kaka == true) {
+    alert("selecionaste a kaka");
+  } else if (chipa == true) {
+    alert("selecionaste a chipa");
+  } else{
+    alert("seleciona una mascota")
+  }
+}
 
+window.addEventListener("load", iniciarJuego);
 
 //Eventos que podemos escuchar con: addEventListener("")
 
